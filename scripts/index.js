7734ad6cf21f998cfd940f88cc7be3452d7e23cc
Aleksandr Tokmashov <alex.tokmashov@yandex.ru>
2024-05-16 23:18:17 +0300

    adding functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function createCard(cardName, cardLink, deleteFunc) {
+  card = cardTemplate.querySelector('.places__item').cloneNode(true);
+  card.querySelector('.card__image').src = cardLink;
+  card.querySelector('.card__title').textContent = cardName;
+  placesList.append(card);
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteFunc);
+
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard(evt) {
+  const eventTarget = evt.target;
+  const targetParent = eventTarget.parentElement
+  targetParent.remove()
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(item) {
+  createCard(item.name, item.link, deleteCard);
+})
